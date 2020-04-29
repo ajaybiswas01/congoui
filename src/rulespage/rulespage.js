a5c0361d131221a72939464ac6d0f5a955c7cf70
Ajay Biswas <ajay.b.tpr@pwc.com>
2020-04-30 01:32:19 +0530

    col condition

--- src/rulespage/rulespage.js
+++ src/rulespage/rulespage.js
@@ -8,8 +8,7 @@ import makeAnimated from 'react-select/animated';
 
 
 const animatedComponents = makeAnimated();
-const rulesdataVal = Object.values(data);
-// const tabaleTdData =[];
+
 export default class Rulespage extends Component {
     constructor(props) {
         super(props);
@@ -20,6 +19,7 @@ export default class Rulespage extends Component {
         this.addClickRow = this.addClickRow.bind(this);
         this.state = {
             selectedOption: null,
+            ruleName: '',
             leftSideData: Object.values(data),
             rulesList: [],
             rulesdata: [],
@@ -59,7 +59,7 @@ export default class Rulespage extends Component {
             objVal: new Array(),
             escuSelBox: new Array(),
             escuSelBoxStore: null,
-            objDataSel: null,
+            objDataSel: [],
 
         };
         for (let c = 0; c < this.state.leftSideData.length; c++) {
@@ -175,11 +175,11 @@ export default class Rulespage extends Component {
         this.setState({ tableDataListNew: this.state.tableDataListNew });
         console.log('rulesList', this.state.tableDataListNew)
     }
-    // handleChange = (e) => {
-    //     this.setState({
-    //         [e.target.name]: e.target.value
-    //     });
-    // }
+    handleinputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
     // onSubmit = (e) => {
 
     //     e.preventDefault();
@@ -202,15 +202,22 @@ export default class Rulespage extends Component {
     // }
 
     onOptionChange = (selectedOption) => {
-        this.state.selJobList = []
-        for (let s = 0; s < selectedOption.length; s++) {
-            const element = selectedOption[s].value;
-            this.state.selJobList.push(element)
-            console.log('element', element)
+        const selJobList = []
+        if (selectedOption === null) {
+            console.log('No Sel value')
+        } else {
+            console.log('Sel value have')
+            for (let s = 0; s < selectedOption.length; s++) {
+                const element = selectedOption[s].value;
+                selJobList.push(element)
+                console.log('element', element)
+            }
         }
-        const result = Object.keys(this.state.selJobList).map((key) => this.state.selJobList[key])
-        console.log('selectedOption', result)
-        console.log('result', this.state.selJobList)
+
+        // const result = Object.keys(this.state.selJobList).map((key) => this.state.selJobList[key])
+        // console.log('selectedOption', result)
+        // console.log('result', this.state.selJobList)
+        this.state.selJobList = selJobList
         this.state.objDataSel = this.state.selJobList
         this.setState({
             selJobList: this.state.selJobList,
@@ -226,7 +233,7 @@ export default class Rulespage extends Component {
 
     createUI() {
         return this.state.users.map((el, i) => (
-            <div key={i} className="rowDiv d-flex justify-content-between mb-3">
+            <div key={i} className="rowDiv d-flex justify-content-between mb-3" onClick={this.inputClick(this, i)}>
                 <div className="row flex-fill">
                     <div className="col-md-6">
                         <input className="form-control" placeholder="Column Name" name="colName" value={el.colName || ''} onChange={this.handleChange.bind(this, i)} />
@@ -248,8 +255,6 @@ export default class Rulespage extends Component {
         let users = [...this.state.users];
         users[i] = { ...users[i], [name]: value };
         this.setState({ users });
-        this.state.addColShow = true
-        console.log('users', users)
     }
 
     removeClick(i) {
@@ -257,128 +262,148 @@ export default class Rulespage extends Component {
         users.splice(i, 1);
         this.setState({ users });
     }
-
+    inputClick(i, e) {
+        this.state.addColShow = true
+    }
     handleSubmit(event) {
-        this.state.addRowShow = true;
+
         this.state.errorMsg = false
         //alert('A name was submitted: ' + JSON.stringify(this.state.users));
         event.preventDefault();
         console.log('data', this.state.selJobList)
-        if (this.state.selJobList === null) {
-            //return this.state.fieldsList
-            this.state.errorMsg = true
-        } else {
-            if (this.state.users === null) {
-                console.log('blank')
-            }else{
-                console.log('no blank')
-            }
-
-            this.state.colData = this.state.users
-            this.state.vData = new Array()
-            this.state.objVal = new Array()
-            this.state.fieldsListThead = []
-            this.state.valColObj = new Array()
-            for (let x = 0; x < this.state.colData.length; x++) {
-                const element = this.state.colData[x];
-                this.state.vData.push(element)
-                this.state.vData[x]['exclude'] = this.state.selJobList;
-            }
-
-            for (let w = 0; w < this.state.selJobList.length; w++) {
-                const element = this.state.selJobList[w];
-                console.log('element bbb', element)
-                this.state.escuSelBox.push({ 'label': this.state.selJobList[w], 'value': w })
-            }
-            this.state.escuSelBoxStore = this.state.escuSelBox;
-            this.state.newarrval.push(this.state.vData)
-            this.state.newarrvalThead.push(this.state.vData)
-            this.state.fieldsList = this.state.newarrval;
-            for (let y = 0; y < this.state.colData.length; y++) {
-                this.state.colData[y] = []
-                this.state.colData.splice(event, this.state.colData.length);
-                for (let m = 0; m < this.state.colData.length; m++) {
-                    // this.state.colData[m].colName=''
-                    // this.state.colData[m].colValue=''
-                    console.log('m', this.state.colData[m])
+ 
+            console.log('no Blank', this.state.users)
+            if (this.state.users[0].colName == '') {
+                console.log('no user value', Object.values(this.state.users))
+            } else {
+                
+                for (let e = 0; e < this.state.users.length; e++) {
+                    if (this.state.users[e].colName==null) {
+                        alert('Column name empty')
+                        return false
+                    }
                 }
-                this.state.colData[y] = Object.values(this.state.users)
-            }
-            for (let b = 0; b < this.state.fieldsList.length; b++) {
-                const valueResult = Object.values(this.state.fieldsList[b])
-                console.log('valueResult', valueResult)
-                for (let u = 0; u < valueResult.length; u++) {
-                    this.state.objVal.push(valueResult[u])
-                }
+                var names = [];
+                this.state.users.forEach((item) => {
+                    if (!names.some(val => val === item)) {
+                      names.push(item.colName);
+                    }
+                  })
+                  console.log('names', names)
+                  
+                  let map = {};
+                  let result = false;
+                  for(let i = 0; i < names.length; i++) {
+                     if(map[names[i]]) {
+                        result = true;
+                        break;
+                     }
+                     map[names[i]] = true;
+                  }
+                  if(result) {
+                     alert('Column name already exist')
+                     return false
+                  } else {
+                    console.log('val not mathch --->>>');
+                  }
 
 
-            }
-            console.log('u', this.state.objVal)
-            // this.state.colData.push(this.state.objVal)
-            // this.state.colData.push(Object.keys(this.state.fieldsList).map((key)=>this.state.fieldsList[key]))
-            //this.state.colData[0]=this.state.fieldsList
 
-            // if (this.state.objVal[0]['colName'] === 'Col name1') {
-            //     console.log('colName true')
-            // } else {
-            //     console.log('colName false')
-            // }
-            this.state.fieldsListThead[0] = Object.values(this.state.objVal)
-            for (let c = 0; c < this.state.fieldsListThead[0].length; c++) {
-                for (let j = 0; j < this.state.users.length; j++) {
-                    if (this.state.fieldsListThead[0]['colName'] === this.state.users[j]['colName']) {
-                        console.log('colName true')
-                    } else {
-                        console.log('colName false')
+                for (let index = 0; index < this.state.objVal.length; index++) {
+                    const element = this.state.objVal[index].colName;
+                    for (let u = 0; u < this.state.users.length; u++) {
+                        if (this.state.users[u].colName == element) {
+                            console.log('val mathch');
+                            alert('Column name already exist', JSON.stringify(this.state.users[u].colName))
+                            return false
+                        }
                     }
                 }
 
+                this.state.addRowShow = true;
+                this.state.colData = this.state.users
+                this.state.vData = new Array()
+                this.state.objVal = new Array()
+                this.state.fieldsListThead = []
+                this.state.valColObj = new Array()
+                for (let x = 0; x < this.state.colData.length; x++) {
+                    const element = this.state.colData[x];
+                    this.state.vData.push(element)
+                    this.state.vData[x]['exclude'] = this.state.selJobList;
+                }
 
+                for (let w = 0; w < this.state.selJobList.length; w++) {
+                    this.state.escuSelBox.push({ 'label': this.state.selJobList[w], 'value': w })
+                }
+                this.state.escuSelBoxStore = this.state.escuSelBox;
+                this.state.newarrval.push(this.state.vData)
+                this.state.newarrvalThead.push(this.state.vData)
+                this.state.fieldsList = this.state.newarrval;
+                for (let y = 0; y < this.state.colData.length; y++) {
+                    this.state.colData[y] = []
+                    this.state.colData.splice(event, this.state.colData.length);
+                    this.state.colData[y] = Object.values(this.state.users)
+                }
+                for (let b = 0; b < this.state.fieldsList.length; b++) {
+                    const valueResult = Object.values(this.state.fieldsList[b])
+                    for (let u = 0; u < valueResult.length; u++) {
+                        this.state.objVal.push(valueResult[u])
+                    }
+                }
+                console.log('u', this.state.objVal)
+                this.state.fieldsListThead[0] = Object.values(this.state.objVal)
+                for (let c = 0; c < this.state.fieldsListThead[0].length; c++) {
+                    for (let j = 0; j < this.state.users.length; j++) {
+                        if (this.state.fieldsListThead[0]['colName'] === this.state.users[j]['colName']) {
+                            console.log('colName true')
+                        } else {
+                            console.log('colName false')
+                        }
+                    }
+
+
+                }
+                for (let v = 0; v < this.state.fieldsList.length; v++) {
+                    this.state.fieldsList = this.state.fieldsListNew
+                    this.state.fieldsList[v] = Object.values(this.state.objVal)
+                }
             }
-            console.log('aas', this.state.fieldsListThead)
-            for (let v = 0; v < this.state.fieldsList.length; v++) {
-                this.state.fieldsList = this.state.fieldsListNew
-                this.state.fieldsList[v] = Object.values(this.state.objVal)
-            }
-        }
+
         this.setState({
             colData: this.state.colData,
             fieldsList: this.state.fieldsList,
             fieldsListThead: this.state.fieldsListThead,
-            escuSelBox: this.state.escuSelBox
+            escuSelBox: this.state.escuSelBox,
+            selJobList:this.state.selJobList
         })
     }
     addClickRow(e) {
         var nextState = this.state;
 
         nextState.fieldsListNew.push(this.state.objVal);
-        // for (let g = 0; g < nextState.fieldsList.length-1; g++) {
-        //     const element = nextState.fieldsList[g];
-        //     this.state.appendData.push(element)
-        //     for (let d = 0; d <nextState.fieldsList[g].length; d++) {
-        //         this.state.appendData[g][d].colValue=''
-        //     }
 
-        // }
-        console.log('element', e)
-        for (let s = 1; s < nextState.fieldsListNew.length; s++) {
-            const element = nextState.fieldsListNew[s];
-            // element[s].colName=''
-            //element[s].colValue=''
-            console.log('ee', element)
-        }
         nextState.fieldsList = nextState.fieldsListNew
+        const resultss = Object.keys(this.state.fieldsList).map((key) => this.state.fieldsList[key])
+        resultss.forEach((element, i) => {
+            for (let index = 0; index < element.length; index++) {
+                const el = element[index]
+                el.colValue=''
+            }
+        });
+        
+        console.log('resultss', resultss)
         this.setState({
             fieldsList: nextState.fieldsList,
             fieldsListNew: nextState.fieldsListNew
         })
-        console.log('new row', nextState.fieldsList)
+        
+        console.log('new row', Object.values(nextState.fieldsList[1]))
     }
 
 
 
     render() {
-        this.state.selJobList = this.state.objDataSel
+        this.state.addColShow = true
         for (let v = 0; v < this.state.rulesList.length; v++) {
             this.state.tabaleTdData.push(Object.values(this.state.rulesList[v]))
         }
@@ -423,7 +448,8 @@ export default class Rulespage extends Component {
                             <div className="form-group d-flex align-items-center">
                                 <label className="form-label">Rule name<em>*</em></label>
                                 <div className="input-box">
-                                    <input type="text" value="" className="form-control" />
+                                    {/* <input type="text" value="" className="form-control" /> */}
+                                    <input type="text" name='ruleName' value={this.state.ruleName} onChange={e => this.handleinputChange(e)} className="form-control" />
                                 </div>
                             </div>
                             <div className="form-group d-flex align-items-center">
@@ -437,7 +463,7 @@ export default class Rulespage extends Component {
                                             options={this.state.joblist} onChange={e => this.onOptionChange(e)}
                                         />
                                     </div>
-                                    {this.state.errorMsg ? <div className="alert alert-danger" role="alert" >Choose job name</div> : null}
+                                    {/* {this.state.errorMsg ? <div className="alert alert-danger" role="alert" >Choose job name</div> : null} */}
                                 </div>
 
 
@@ -526,7 +552,7 @@ export default class Rulespage extends Component {
                                                 {
                                                     r.map((v, b) => {
                                                         return (v !== undefined &&
-                                                            <td key={b}><input value={v.colValue} className="form-control" /></td>
+                                                            <td key={b}><input value={v.colValue} onChange={e => this.handleinputChange(e)} className="form-control" /></td>
 
                                                         )
 
